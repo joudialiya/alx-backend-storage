e3f4fd980ed2b6c1f5d8c4f823ad25b821fab374
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-16 15:46:12 +0000

    0.

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,10 +1,3 @@
-# THIS SQL SCRIPT CREATES USER TABLE IN HELBERTON DATABASE
-# create databse
-CREATE DATABASE IF NOT EXISTS holberton;
-# use that database
-USE holberton;
-# dropt table is exists
-DROP TABLE users;
 # create the users table
 CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
