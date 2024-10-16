2429ba1f574315808de65afa14b8adbf691c2926
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-16 15:55:10 +0000

    0.1.2.

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,9 +1,3 @@
--- create databse
-CREATE DATABASE IF NOT EXISTS holberton;
--- use that database
-USE holberton;
--- drop table is exists
-DROP TABLE users;
 -- create the users table
 CREATE TABLE IF NOT EXISTS users (
   id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,3 +1,3 @@
-# Select group
+-- Select group
 SELECT origin, SUM(id) AS nb_fans
 FROM metal_bands GROUP BY origin;
