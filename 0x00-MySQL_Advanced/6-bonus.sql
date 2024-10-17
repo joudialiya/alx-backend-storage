837a1db6d218a77929e04d6f4743f09651cb49c1
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-17 10:54:57 +0000

    7.

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -5,11 +5,11 @@ CREATE PROCEDURE AddBonus
 (IN user_id INTEGER, IN project_name VARCHAR(255), IN score INTEGER)
 BEGIN
   INSERT INTO projects(name)
-    SELECT project_name FROM DUAL
-    WHERE NOT EXISTS(
-      SELECT *
-      FROM projects
-      WHERE name = project_name);
+  SELECT project_name FROM DUAL
+  WHERE NOT EXISTS(
+    SELECT *
+    FROM projects
+    WHERE name = project_name);
 
   INSERT INTO corrections(user_id, project_id, score) 
   VALUES (
