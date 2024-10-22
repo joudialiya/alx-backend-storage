202f52d50eac8ff39e28868fcd5f368c18e6491b
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-22 11:09:55 +0000

    9. an other update

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 """Insert using pycode"""
+import pymongo.collection
 
 
-def insert_school(mongo_collection, **kwargs):
-    """
-    Insert to a collection function
-    :return int
-    """
-    return mongo_collection.insert_one(kwargs).__inserted_id
+def insert_school(
+        mongo_collection: pymongo.collection.Collection,
+        **kwargs):
+    """Insert to a collection function"""
+    document = mongo_collection.insert_one(kwargs)
+    return document.__inserted_id
