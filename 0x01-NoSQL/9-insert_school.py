eb6d42f3cd19ddf691747e4bef46ff1840079f93
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-22 10:58:58 +0000

    9. add type hinting

--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 """Insert using pycode"""
 from pymongo.collection import Collection
+import typing
 
 
-def insert_school(mongo_collection: Collection, **kwargs):
+def insert_school(
+        mongo_collection: Collection,
+        **kwargs: typing.Dict[str, typing.Any]):
     """Intert function"""
     return mongo_collection.insert_one(kwargs).__inserted_id
