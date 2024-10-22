6ad4d01ea33f51eea809fa6fedfe45a3fb0fc7d5
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-22 11:46:06 +0000

    12.

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""Get Logs"""
+import pymongo
+import pymongo.collection
+
+
+client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+nginx_collection = client.logs.nginx
+print('{:d} logs'.format(nginx_collection.count_documents({})))
+for method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
+    print('\tmethode {}: {}'.format(
+        method,
+        nginx_collection.count_documents({'method': method})))
