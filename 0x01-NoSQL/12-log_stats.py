2c260ee77ad8103282ed80feca743083dccd4ea6
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-22 11:51:26 +0000

    12.

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,11 +3,14 @@
 import pymongo
 import pymongo.collection
 
-
-client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
-nginx_collection = client.logs.nginx
-print('{:d} logs'.format(nginx_collection.count_documents({})))
-for method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
-    print('\tmethod {}: {}'.format(
-        method,
-        nginx_collection.count_documents({'method': method})))
+if __name__ == '__main__':
+    client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
+    nginx_collection = client.logs.nginx
+    print('{:d} logs'.format(nginx_collection.count_documents({})))
+    print('Methods:')
+    for method in ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']:
+        print('\tmethod {}: {}'.format(
+            method,
+            nginx_collection.count_documents({'method': method})))
+    print('{} status check'.format(
+        nginx_collection.count_documents({'path': '/status'})))
