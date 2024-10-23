f6447aba63b9799ca2394476e4113bf775939d9b
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-23 14:10:18 +0000

    5.

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -8,19 +8,17 @@ from typing import Callable
 
 def keep_count_decorator(fn: Callable) -> Callable:
     """ Decorator to count page access """
-    client = redis.Redis()
 
     @wraps(fn)
     def wrapper(url: str) -> str:
-        """ Wrapper function """
+        """ wrapper logic function """
+        client = redis.Redis()
         client.incr(f'count:{url}')
-
-        cached_page = client.get(url)
+        cached_page = client.get(f'{url}')
         if cached_page:
             return cached_page.decode('utf-8')
         response = fn(url)
-        client.set(f'count:{url}', 0)
-        client.set(url, response, 10)
+        client.set(f'{url}', response, 10)
         return response
     return wrapper
 
