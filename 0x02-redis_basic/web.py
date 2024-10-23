26f28c3e493fb2becacdd8d81c856c4d7d1e6959
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-23 13:37:24 +0000

    web.py

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""Implementing an expiring web cache and tracker"""
+import redis
+import requests
+from typing import Callable
+
+
+def keep_count_decorator(fn: Callable) -> Callable:
+    client = redis.Redis()
+
+    def wrapper(url):
+        key = "count:{}".format(url)
+        client.incr(key)
+
+        cached_page = client.get(url)
+        if cached_page:
+            return cached_page.decode('utf-8')
+        response = fn(url)
+        client.set(url, response, 10)
+        return response
+    return wrapper
+
+
+@keep_count_decorator
+def get_page(url: str) -> str:
+    """keep track of reqs"""
+    requests.get(url)
