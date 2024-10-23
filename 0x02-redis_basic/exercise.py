507cbca85e35d1bc474d7b718b0672231f017ad3
Lagzal <mlagzal.sayhi@gmail.com>
2024-10-23 09:18:42 +0000

    0.

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Exercise"""
+import redis
+import typing
+import uuid
+
+
+class Cache:
+    """Cache class"""
+    def __init__(self) -> None:
+        """Constructor"""
+        self._redis: redis.Redis = redis.Redis('127.0.0.1', 6379)
+
+    def store(
+            self,
+            data: typing.Union[str, bytes, int, float]) -> str:
+        id: str = str(uuid.uuid1())
+        self._redis.set(id, data)
+        return id
